9ef6c29c0f0ec333a3836ffe178ed075c8e8aa39
Nukr <nukrs.w@gmail.com>
2015-05-03 19:49:48 +0800

    finish meals and bills

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import compose from 'koa-compose';
 import users from './users';
 import bills from './bills';
+import meals from './meals';
 
-export default compose([users, bills]);
+export default compose([users, bills, meals]);
--- routes/meals.js
+++ routes/meals.js
@@ -2,13 +2,14 @@ import r from 'rethinkdb';
 import Router from 'koa-router';
 
 let router = Router();
-router.get('/users', get);
+router.get('/meals', get);
 
 function * get (next) {
-  var cursor = yield r.table('bills').run(this._rdbConn);
-  this._result = yield cursor.toArray();
+  var cursor = yield r.table('meals').run(this._rdbConn);
+  this._results = yield cursor.toArray();
   yield next;
 }
 
 export default router.middleware();
+
 
