9e3f4c48d97add41e0409a8b791577f771b752cf
Nukr <nukrs.w@gmail.com>
2015-06-17 10:03:50 +0800

    progress

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,13 @@
 import Router from 'koa-router';
+import parse from 'co-body'
 
 let router = Router();
 
-router.get('/bills', get);
+router.post('/login', login);
+
+function * login () {
+  let {username, password} = yield parse.json(this)
+  this.body = `${username} - ${password}`
+}
 
 export default router.middleware();
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ import users from './users';
 import bills from './bills';
 import meals from './meals';
 import root from './root';
+import auth from './auth';
 
-export default compose([root, users, bills, meals]);
+export default compose([root, users, bills, meals, auth]);
